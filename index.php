f87119b5a8ead9fbce8464776931ced5b625732f
Matt David <mcdav3636@gmail.com>
2017-11-07 22:31:39 -0700

    added image

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
-<!php>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8" lang="en-Us">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- CSS style link -->
+    <link rel="stylesheet" href="style/style.css" type="text/css" />
+
+</head>
+<img src="images/dirtbike.jpg" class="button" alt="dirt bike image">
+<body>
+<script src="script.js"></script>
+</body>
+</html>
